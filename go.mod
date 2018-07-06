3c411ce51cfcee204cbdebd405f71cdab1dd653b
matsu-chara <matsuy00@gmail.com>
2018-07-06 23:25:13 +0900

    serve without Resolver

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/matsu-chara/suburi-graphql
 
 require (
 	github.com/go-redis/redis v0.0.0-20180629081417-5db3ab16e503
+	github.com/gorilla/websocket v1.2.0
+	github.com/rs/cors v1.4.0
 	github.com/tinrab/retry v1.0.0
 	github.com/vektah/gqlgen v0.0.0-20180706040623-325c45a40b41
 )
--- server/graphql.go
+++ server/graphql.go
@@ -2,11 +2,16 @@
 package server
 
 import (
+	"fmt"
+	"net/http"
 	"sync"
 	"time"
 
 	"github.com/go-redis/redis"
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
 	"github.com/tinrab/retry"
+	"github.com/vektah/gqlgen/handler"
 )
 
 type graphQLServer struct {
@@ -16,6 +21,7 @@ type graphQLServer struct {
 	mutex           sync.Mutex
 }
 
+// NewGraphQLServer .
 func NewGraphQLServer(redisURL string) (*graphQLServer, error) {
 	client := redis.NewClient(&redis.Options{
 		Addr: redisURL,
@@ -31,4 +37,23 @@ func NewGraphQLServer(redisURL string) (*graphQLServer, error) {
 		userChannels:    map[string]chan string{},
 		mutex:           sync.Mutex{},
 	}, nil
+}
+
+// Serve .
+func (s *graphQLServer) Serve(route string, port int) error {
+	mux := http.NewServeMux()
+	mux.Handle(
+		route,
+		handler.GraphQL(MakeExecutableSchema(s),
+			handler.WebsocketUpgrader(websocket.Upgrader{
+				CheckOrigin: func(r *http.Request) bool {
+					return true
+				},
+			}),
+		),
+	)
+	mux.Handle("/playground", handler.Playground("GraphQL", route))
+
+	handler := cors.AllowAll().Handler(mux)
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), handler)
 }
