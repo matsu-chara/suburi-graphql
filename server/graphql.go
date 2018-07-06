de45fe5c771597218e9f42040c2a9a1d98999a88
matsu-chara <matsuy00@gmail.com>
2018-07-06 23:17:59 +0900

    new client

--- server/graphql.go
+++ server/graphql.go
@@ -3,17 +3,32 @@ package server
 
 import (
 	"sync"
+	"time"
 
 	"github.com/go-redis/redis"
+	"github.com/tinrab/retry"
 )
 
 type graphQLServer struct {
-	redisClint      *redis.Client
+	redisClient     *redis.Client
 	messageChannels map[string]chan Message
 	userChannels    map[string]chan string
 	mutex           sync.Mutex
 }
 
 func NewGraphQLServer(redisURL string) (*graphQLServer, error) {
-	return nil, nil
+	client := redis.NewClient(&redis.Options{
+		Addr: redisURL,
+	})
+
+	retry.ForeverSleep(2*time.Second, func(_ int) error {
+		_, err := client.Ping().Result()
+		return err
+	})
+	return &graphQLServer{
+		redisClient:     client,
+		messageChannels: map[string]chan Message{},
+		userChannels:    map[string]chan string{},
+		mutex:           sync.Mutex{},
+	}, nil
 }
