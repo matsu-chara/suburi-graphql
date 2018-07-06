6f761da976e761d20b01e11e782df8fb97a6b5c9
matsu-chara <matsuy00@gmail.com>
2018-07-06 23:05:40 +0900

    go generate server/graphql.go

---
+++ server/graphql.go
@@ -0,0 +1,15 @@
+//go:generate gqlgen -schema ./schema.graphql
+package server
+
+import (
+	"sync"
+
+	"github.com/go-redis/redis"
+)
+
+type graphQLServer struct {
+	redisClint      *redis.Client
+	messageChannels map[string]chan Message
+	userChannels    map[string]chan string
+	mutex           sync.Mutex
+}
