bfd6a9309e25e3f480a849289ad2a1fff7d22555
matsu-chara <matsuy00@gmail.com>
2018-07-07 14:16:37 +0900

    add graphql

---
+++ graph-micro/graphql/app.dockerfile
@@ -0,0 +1,15 @@
+FROM golang:1.10.2-alpine3.7 AS build
+RUN apk --no-cache add gcc g++ make ca-certificates
+WORKDIR /go/src/github.com/tinrab/spidey/graphql
+COPY vendor ../vendor
+COPY account ../account
+COPY catalog ../catalog
+COPY order ../order
+COPY graphql ./
+RUN go build -o /go/bin/app
+
+FROM alpine:3.7
+WORKDIR /usr/bin
+COPY --from=build /go/bin .
+EXPOSE 8080
+CMD ["app"]
---
+++ graph-micro/graphql/graph/graph.go
@@ -0,0 +1,43 @@
+//go:generate gqlgen -schema ../schema.graphql
+package graph
+
+import (
+	"github.com/tinrab/spidey/account"
+	"github.com/tinrab/spidey/catalog"
+	"github.com/tinrab/spidey/order"
+)
+
+type GraphQLServer struct {
+	accountClient *account.Client
+	catalogClient *catalog.Client
+	orderClient   *order.Client
+}
+
+func NewGraphQLServer(accountUrl, catalogURL, orderURL string) (*GraphQLServer, error) {
+	// Connect to account service
+	accountClient, err := account.NewClient(accountUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	// Connect to product service
+	catalogClient, err := catalog.NewClient(catalogURL)
+	if err != nil {
+		accountClient.Close()
+		return nil, err
+	}
+
+	// Connect to order service
+	orderClient, err := order.NewClient(orderURL)
+	if err != nil {
+		accountClient.Close()
+		catalogClient.Close()
+		return nil, err
+	}
+
+	return &GraphQLServer{
+		accountClient,
+		catalogClient,
+		orderClient,
+	}, nil
+}
